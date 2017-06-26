30dda23cb2c91cb59f5b8ada56cceb166725be24
pablostellet <pablo.dias@bemind.com.br>
2017-06-26 17:26:40 -0300

    creating input and form

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>.....</p>
+        <p>.....</p>
+        <p>.....</p>
+
     </div>
 )
---
+++ src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>sou uma lista</h1>
+    </div>
+)
