67416eed2ee01b60ab797a15fdb4fe8365f89e37
= <pablo.dias@bemind.com.br>
2017-06-26 16:25:42 -0300

    criacao de menu e controle de rotas pelo react

---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Tarefas</h1>
+            </div>
+        )
+    }
+}
