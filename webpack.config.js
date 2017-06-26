21d089a0c7e95988f5a8bf45b1c75d93135dd670
= <pablo.dias@bemind.com.br>
2017-06-26 15:35:55 -0300

    termino de configuracao reactjs front

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
         filename: './app.js'
     },
     devServer: {
-        port:8080,
-        contentBase: './public'
+        port: 8080,
+        contentBase: './public',
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
@@ -17,19 +17,19 @@ module.exports = {
             modules: __dirname + '/node_modules'
         }
     },
-    plugin:[
-        new ExtractTextPlugin('app.css')        
+    plugins: [
+        new ExtractTextPlugin('app.css')
     ],
     module: {
-        loaders:[{
-            test: /.js[x]?$/,        
+        loaders: [{
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
