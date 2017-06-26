2c98fa3af565ea64e0a90fae5d579f26ca314821
pablostellet <pablo.dias@bemind.com.br>
2017-06-26 18:12:44 -0300

    componentization of buttons and creating bootstrap class dynamically

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div role='form' className='<todoForm></todoForm>'>
-        <div className="col-xs-12 col-sm-9 col-md-10">
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa' />
-        </div>
 
-        <div className="col-xs-12 col-sm3 col-md-2">
-            <button className='btn btn-primary'>
-                <i className="fa fa-plus"></i>
-            </button>
-        </div>
+        <Grid cols="12 9 10">
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa' />
+        </Grid>
+
+        <Grid cols="12 3 2">
+            <IconButton style='primary' icon='plus'></IconButton>
+        </Grid>
 
     </div>
 )
